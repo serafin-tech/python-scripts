5ce60e2f740fea68e2d43a6c3a711e90cc8b72a6
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2023-12-27 00:22:00 +0100

    skelton script update

--- simple/skelton.py
+++ simple/skelton.py
@@ -1,67 +1,96 @@
 #!/usr/bin/env python3
-
 """
 skelton script to speed-up development
 """
-
+from collections import namedtuple
 from pprint import pprint
 import argparse
 import csv
 import json
+from typing import Dict, List
+
+CLI_PARAMS = [
+    {
+        'name': 'input_file',
+        'help': 'file to be processed'
+    },
+    {
+        'name': 'output_file',
+        'help': 'file to write output to'
+    }
+]
+
+CliParams = namedtuple('CliParams', [item['name'] for item in CLI_PARAMS])
 
 
-def get_file_content(fname):
-    with open(fname, mode='r', encoding='utf-8') as file:
+def get_file_content(file_name):
+    """
+    Reads text file content and returns list containing all lines from the file
+    :param file_name:
+    :return:
+    """
+    with open(file_name, mode='r', encoding='utf-8') as file:
         return [line.rstrip() for line in file.readlines()]
 
 
-def json2dict(fname: str):
+def json2dict(file_name: str) -> Dict:
     """
     function to serialize json into dictionary
+    :param file_name:
+    :return deserialized dictionary:
     """
     try:
-        with open(fname, 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except Exception as e:
-        print(e)
-        return {}
+        with open(file_name, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except IOError as err:
+        print(err)
+        return {'error': 'file operation error'}
+    except json.JSONDecodeError as err:
+        print(err)
+        return {'error': 'JSON parsing error'}
 
 
-def write_csv(fname: str, content: list):
+def write_csv(file_name: str, content: list):
     """
     function to write list of lists as CSV file
     """
-    with open(fname, "w", encoding="utf-8", newline='') as file:
+    with open(file_name, 'w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
-
         writer.writerows(content)
 
 
-def args_parser():
+def args_parser(parameters_spec: List[Dict]) -> CliParams:
     """
     argument parser, returns arguments as a tupple
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('infile',
-                        help='file to be processed')
-    parser.add_argument('outfile',
-                        help='file to write output to')
-    parser.add_argument('-v', '--verbose',
+    parser.add_argument('-v',
+                        '--verbose',
                         action='store_true',
                         default=False,
                         help='talkative mode')
 
-    params = parser.parse_args()
+    for i in parameters_spec:
+        parser.add_argument(i['name'], help=i['help'])
 
-    return params.infile, params.outfile
+    parsed_args = parser.parse_args()
+
+    ret_list = []
+    for param in CliParams._fields:
+        ret_list.append(getattr(parsed_args, param))
+
+    return CliParams(*ret_list)
 
 
-def main(inf: str, outf: str):
+def main(input_file: str, output_file: str):
     """
-    main function of the skelton
+    main function of the script
+    :param input_file:
+    :param output_file:
+    :return:
     """
-    with open(inf, "r", encoding="utf-8") as file:
+    with open(input_file, 'r', encoding='utf-8') as file:
         for line in file:
             pprint(line)
 
@@ -71,10 +100,10 @@ def main(inf: str, outf: str):
         ['a3', 'b3', 'c3'],
     ]
 
-    write_csv(outf, payload)
+    write_csv(output_file, payload)
 
 
 if __name__ == "__main__":
-    infile, outfile = args_parser()
+    args = args_parser(CLI_PARAMS)
 
-    main(infile, outfile)
+    main(args.input_file, args.output_file)
