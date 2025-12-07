27178f2286f388aee2fbfd1581be223b86f57930
Jaroslaw Wencel <jaroslaw@wencel.it>
2025-12-07 17:59:27 +0100

    script for reading data from RDAP

--- simple/README.md
+++ simple/README.md
@@ -1,8 +1,9 @@
 # Simple scripts
 
 main assumptions:
+
 - one script - one task
-- standard library mostly
+- standard library mostly, in case other modules are needed - modules to be installed from standard Ubuntu repository (latest LTS version)
 
 Scripts:
 
@@ -12,5 +13,6 @@ Scripts:
 4. [`gcp_get_api_addresses.py`](gcp_get_api_addresses.py) - get and process Google Cloud Platform API addresses to import them to router
 5. [`github_get_user_keys.py`](github_get_user_keys.py) - get and process GitHub user keys to import them to router
 6. [`log-loader.py`](log-loader.py) - provides functionality to forward log entries from a specified log file to a syslog server
-7. [`skelton.py`](skelton.py) - skelton script to speed-up development
-8. [`uptimerobot_get_probe_addresses.py`](uptimerobot_get_probe_addresses.py) - get and process UptimeRobot probe addresses to import them to router
+7. [`rdap_get_domain_details.py`](rdap_get_domain_details.py) - reads domain details from RDAP database
+8. [`skelton.py`](skelton.py) - skelton script to speed-up development
+9. [`uptimerobot_get_probe_addresses.py`](uptimerobot_get_probe_addresses.py) - get and process UptimeRobot probe addresses to import them to router
---
+++ simple/rdap_get_domain_details.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-function-docstring
+"""
+RDAP information retiver for domain details
+"""
+import argparse
+import json
+import logging
+from pprint import pformat
+
+import httpx
+import validators
+
+def get_file_content(file_name):
+    with open(file_name, mode='r', encoding='utf-8') as file:
+        return [line.rstrip() for line in file.readlines()]
+
+
+def args_parser() -> argparse.Namespace:
+    """
+    CLI argument parser
+    :return: argparse.Namespace with parsed arguments
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',
+                        default=False,
+                        help='talkative mode')
+
+    parser.add_argument('domain',
+                        type=str,
+                        help='Domain name to get RDAP details for')
+
+    return parser.parse_args()
+
+
+def logger_setup(cli_params: argparse.Namespace) -> None:
+    logging.basicConfig(
+        level=(logging.DEBUG if cli_params.verbose else logging.INFO))
+    
+    logging.debug("CLI arguments: %s", pformat(cli_params))
+
+
+def main(domain: str) -> None:
+    logging.debug("Getting RDAP details for domain: %s", domain)
+
+    validators.domain(domain)
+
+    rdap_data = httpx.get(f"https://rdap.org/domain/{domain}", follow_redirects=True)
+    rdap_data.raise_for_status()
+
+    if rdap_data.status_code == 404:
+        logging.info("Authoritative RDAP source unknown for domain: %s", domain)
+        return
+
+    logging.debug("RDAP data received: %s", pformat(rdap_data.json()))
+
+    nameservers = [item.get('ldhName') for item in rdap_data.json().get('nameservers', [])]
+    
+    registar = [item.get('vcardArray')
+                for item in rdap_data.json().get('entities', []) 
+                if 'registrar' in item.get('roles', [])]
+
+    output_data = {
+        'domain': domain,
+        'nameservers': nameservers,
+        'entities': registar
+    }
+
+    logging.info("RDAP details for domain %s:\n%s", domain, json.dumps(output_data, indent=4))
+
+
+if __name__ == "__main__":
+    args = args_parser()
+
+    logger_setup(args)
+
+    main(args.domain)
